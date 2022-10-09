296ccfc53a8af6f2d12943d74b4d14bf56d20977
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-10-09 16:20:34 -0300

    Estudando Anatomia de Classes

--- 3.JavaAnatomiaClasses/src/_3_2$Variaveis.java
+++ 3.JavaAnatomiaClasses/src/_3_2$Variaveis.java
@@ -1,6 +1,9 @@
 public class _3_2$Variaveis {
     public static void main(String[] args) {
         
+        /*Caso seja atribuido o nome "final" na frente do tipo da variável, significa que essa variável 
+        é uma constante, ou seja, jamais poderá ser alterada. Ex: final String meuNome = "Wesley"*/
+
         //Tipo | nome | seu valor
         String meuNome = "Wesley";
         int anoFabricacao = 2022;
@@ -9,6 +12,9 @@ public class _3_2$Variaveis {
 
         //Alterando valores de variáveis
         anoFabricacao = 2018;
+        
+        //Imprimindo as variáveis na tela
+        System.out.println(meuNome + " " + anoFabricacao + " " + verdadeira + " " + falso);
 
     }
 
