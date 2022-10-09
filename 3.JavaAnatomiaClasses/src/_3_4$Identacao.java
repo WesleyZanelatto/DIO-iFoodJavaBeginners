ef83d970c7b7e3b542bd213739e667fcad82af54
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-10-09 16:36:06 -0300

    Estudo sobre Identação

--- 3.JavaAnatomiaClasses/src/_3_4$Identacao.java
+++ 3.JavaAnatomiaClasses/src/_3_4$Identacao.java
@@ -1,5 +1,29 @@
 public class _3_4$Identacao {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        /*
+         Indentação (indentation) --> Atalho: Shift+Alt+F
+         Basicamente indentar é um termo utilizado para escrever o código do programa
+         de forma hierárquica, facilitando assim a visualização e o entendimento do programa.
+         */
+
+        /*//Código não Identado 
+        int mediaFinal = 6;
+        if(mediaFinal<6)	
+        System.out.println("REPROVADO"); 
+        else if(mediaFinal==6)
+        System.out.println("PROVA MINERVA"); 
+        else
+        System.out.println("APROVADO");
+        */
+
+        //Código Identado
+        int mediaFinal = 7;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
     }
 }
