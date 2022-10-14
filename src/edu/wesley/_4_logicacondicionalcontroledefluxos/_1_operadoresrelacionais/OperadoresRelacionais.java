5c631215eba16d04ede52df4e8340f8307b18902
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-10-14 19:58:40 -0300

    Estudo sobre Operadores Relacionais.

--- src/edu/wesley/_4_logicacondicionalcontroledefluxos/_1_operadoresrelacionais/OperadoresRelacionais.java
+++ src/edu/wesley/_4_logicacondicionalcontroledefluxos/_1_operadoresrelacionais/OperadoresRelacionais.java
@@ -4,16 +4,54 @@ public class OperadoresRelacionais{
     public static void main(String[] args) {
         
         int i1=10, i2=20;
-        // double f1=4.5, f2=3.5;
-        // char c1='x', c2='y';
-        // String s1="Fulano", s2="Fulano";
-        // boolean b1=true, b2=false;
+        float f1=4.5f, f2=3.5f;
+        double d1=59.9d;
+        char c1='x', c2='y';
+        String s1="Fulano", s2="Fulano", s3="Cicrano";
+        boolean b1=true, b2=false;
+        Long l1=1597l, l2=8997l;
+        byte y1=1;
+        short sh1=25;
 
-        if (i1==i2){
-            System.out.println("i1 é igual a i2");
-        }
-        else{
-            System.out.println("i1 é diferente de i2");
-        }
+        //Variáveis int
+        System.out.println("i1==i2 --> " + (i1==i2)); //Comparadores de Similaridade
+        System.out.println("i1!=i2 --> " + (i1!=i2)); //Comparadores de Similaridade
+        System.out.println("i1>i2 --> " + (i1>i2)); //Comparadores de tamanho
+        System.out.println("i1<=i2 --> " + (i1<=i2));//Comparadores de tamanho
+         
+        //Variáveis float
+        System.out.println("f1==f2 --> " + (f1==f2));
+        System.out.println("f1!=f2 --> " + (f1!=f2));
+        System.out.println("f1>=f2 --> " + (f1>=f2));
+        System.out.println("f1<f2 --> " + (f1<f2));
+        
+        //Variáveis char
+        System.out.println("c1==c2 --> " + (c1==c2));
+        System.out.println("c1!=c2 --> " + (c1!=c2));
+        System.out.println("c1>=c2 --> " + (c1>=c2));
+        System.out.println("c1<=i2 --> " + (c1<=c2));
+        
+        //Variáveis String
+        System.out.println("s1==s2 --> " + (s1==s2));
+        System.out.println("s1==s3 --> " + (s1==s3));
+        System.out.println("s1!=s2 --> " + (s1!=s2));
+       // System.out.println("s1>=s2 --> " + (s1>=s2)); //Vai apresentar erro
+       // System.out.println("s1<s2 --> " + (s1<s2)); //Vai apresentar erro
+        
+        //Variáveis boolean
+        System.out.println("b1==b2 --> " + (b1==b2));
+        System.out.println("b1!=b2 --> " + (b1!=b2));
+       // System.out.println("b1>b2 --> " + (b1>b2)); //Vai apresentar erro
+       // System.out.println("b1<=i1 --> " + (b1<=i1)); //Vai apresentar erro
+        
+        //Variáveis mistas
+        System.out.println("i2>f1 --> " + (i2>f1));
+        System.out.println("d1==sh1 --> " + (d1==sh1));
+      // System.out.println("s2!=c1 --> " + (s2!=c1)); //Vai apresentar erro
+      // System.out.println("s3!=i1 --> " + (s3!=i1)); //Vai apresentar erro
+        
+        System.out.println("l1==i2 --> " + (l1==i2));
+        System.out.println("l1>=i1 --> " + (l1>=i1));
+        System.out.println("y1!=sh1 --> " + (y1!=sh1));
     }
 }
