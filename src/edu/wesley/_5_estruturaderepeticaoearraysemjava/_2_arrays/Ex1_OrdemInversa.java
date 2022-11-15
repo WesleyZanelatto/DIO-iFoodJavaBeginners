aded0e19896ffdde6cfe094bc6733854f991afc9
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-11-15 19:10:59 -0300

    Exercício Arrays com números inversos.

---
+++ src/edu/wesley/_5_estruturaderepeticaoearraysemjava/_2_arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,41 @@
+package edu.wesley._5_estruturaderepeticaoearraysemjava._2_arrays;
+
+import java.util.Scanner;
+
+/**
+ * <h1>Exercício 1</h1>
+ * Crie um vetor de 6 números inteiros
+ * e mostre-os na ordem inversa.
+ * 
+ * Ex.: 
+ * 
+ * <p>
+ * <b>Note:</b> Sempre leia atentamente a documentação!
+ *
+ * @author Wesley Zanelatto
+ * @version 6.0
+ * @since 15/11/2022
+ */
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+            
+        Scanner ler = new Scanner(System.in);
+
+        int[] vetor = new int [6];
+        int i=0;
+
+        System.out.println("Digite 6 números inteiros: ");
+
+        for(i=0;i<6;i++){
+            vetor[i] = ler.nextInt();
+            
+        }
+
+        System.out.println("\nNúmeros na sequência inversa: ");
+        for(i=5;i>=0;i--){
+            System.out.printf("%d ",vetor[i]);
+        }
+        System.out.print("\n\n");
+
+    }
+}
